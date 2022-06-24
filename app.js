a0d46422fc832eaa4f59f0a377b22574d60b9318
Isaac <otichinazaisaac19@gmail.com>
2022-06-24 13:54:29 +0100

    commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const typed = new Typed(".auto-type", {
   strings: [
-    "Ochu, Dominic Onah.",
-    "a Front-End Engineer[Developer].",
-    "a creative and immovable front-End developer with over 2 years experience in building websites and apps in fast-paced. ",
+    "Oti, Chinazaekpere Isaac.",
+    "a Front-End Developer.",
+    "a creative and immovable front-end developer with almost 2 years experience in building websites and apps in fast-paced. ",
   ],
-  typeSpeed: 150,
-  backSpeed: 150,
+  typeSpeed: 70,
+  backSpeed: 130,
   loop: true,
 });
